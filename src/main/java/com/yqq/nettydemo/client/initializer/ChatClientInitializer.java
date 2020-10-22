ac9002b4e4c1ad832f69471270cb79aa5df1c4ee
yeqiangqiang <18382258214@163.com>
2020-10-22 17:02:46 +0800

    修改客户端与服务端解码器不一致问题

--- src/main/java/com/yqq/nettydemo/client/initializer/ChatClientInitializer.java
+++ src/main/java/com/yqq/nettydemo/client/initializer/ChatClientInitializer.java
@@ -4,8 +4,8 @@ import com.yqq.nettydemo.client.handler.ChatClientHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
@@ -22,9 +22,7 @@ public class ChatClientInitializer extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
 
-        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE , 0 , 4
-                , 0 , 4));
-        pipeline.addLast(new LengthFieldPrepender(4));
+        pipeline.addLast(new DelimiterBasedFrameDecoder(4096 , Delimiters.lineDelimiter()));
         pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
         pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
         pipeline.addLast(new ChatClientHandler());
