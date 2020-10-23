7be5da86ab65295e5179f042738fb5e20bffa8b6
yeqiangqiang <18382258214@163.com>
2020-10-23 10:15:09 +0800

    比较handlerAdded、channelRegistered、channelActive三个方法的触发时机

--- src/main/java/com/yqq/nettydemo/server/handler/HttpServerHandler.java
+++ src/main/java/com/yqq/nettydemo/server/handler/HttpServerHandler.java
@@ -17,6 +17,37 @@ import io.netty.util.CharsetUtil;
  * @Time:14:20
  */
 public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+    }
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
         if(httpObject instanceof HttpRequest){
