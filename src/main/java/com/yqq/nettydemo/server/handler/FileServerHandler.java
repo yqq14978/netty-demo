c801f8b2235937c453026091cf8233d2280debf3
yeqiangqiang <18382258214@163.com>
2020-12-17 17:04:42 +0800

    完成服务端接收文件代码

--- src/main/java/com/yqq/nettydemo/server/handler/FileServerHandler.java
+++ src/main/java/com/yqq/nettydemo/server/handler/FileServerHandler.java
@@ -2,8 +2,13 @@ package com.yqq.nettydemo.server.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.multipart.FileUpload;
+import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -16,9 +21,59 @@ import java.nio.channels.FileChannel;
  */
 public class FileServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
+    private static final String PATH = "files";
+    private HttpRequest request;
+    private HttpPostRequestDecoder postRequestDecoder;
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        //因为大文件需要进行分块传输所以第一次传输过来的会是request对象，后续的会是每一个数据块，需要使用HttpContent对象来处理
+        if(msg instanceof HttpRequest){
+            request = (HttpRequest) msg;
+            //如果是post请求需要创建解码器进行处理
+            if(HttpMethod.POST.equals(request.method())){
+                postRequestDecoder = new HttpPostRequestDecoder(request);
+            }
+        } else
+            //对后续的数据块进行处理
+            if (msg instanceof HttpContent && postRequestDecoder != null){
+            HttpContent chunk = (HttpContent) msg;
+            //对数据进行解码
+            postRequestDecoder.offer(chunk);
+            //对数据进行读取
+            readData();
+            //判断是否是最后一部分数据块
+            if(chunk instanceof LastHttpContent){
+                request = null;
+                //摧毁编码器，释放内存
+                postRequestDecoder.destroy();
+                postRequestDecoder = null;
+            }
+        }
 
-//        FileChannel fileChannel =
+    }
+
+    private void readData() {
+        while (postRequestDecoder.hasNext()){
+            InterfaceHttpData httpData = postRequestDecoder.next();
+            if(null != httpData){
+                //写入目标文件夹
+                writeData(httpData);
+            }
+        }
+    }
+
+    private void writeData(InterfaceHttpData data) {
+        //判断是否是文件数据
+        if(InterfaceHttpData.HttpDataType.FileUpload.equals(data.getHttpDataType())){
+            FileUpload fileUpload = (FileUpload) data;
+            if(fileUpload.isCompleted()){
+                try {
+                    fileUpload.renameTo(new File(PATH + fileUpload.getFilename()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
